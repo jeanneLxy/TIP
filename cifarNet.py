3b703d50d3a7173ae56f668bb037286a3e4f3ede
T4ngkeke <96550407+T4ngkeke@users.noreply.github.com>
2023-11-19 17:05:01 +0100

    Update cifarNet.py

--- cifarNet.py
+++ cifarNet.py
@@ -4,7 +4,7 @@ import torch
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.metrics import classification_report
 from torch import nn
-from torch.utils.data import Dataset, WeightedRandomSampler
+from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 import numpy as np
 import pandas as pd
@@ -13,9 +13,30 @@ from torchvision import models, transforms
 from torchvision.io import read_image
 # Ignore warnings
 import warnings
-from torchsampler import ImbalancedDatasetSampler
+
 warnings.filterwarnings("ignore")
 torch.cuda.empty_cache()
+
+
+# function to output csv file
+def outputCSV(folder_path, output_csv, data_list):
+    files = os.listdir(folder_path)
+    with open(output_csv, 'w', newline='') as csvfile:
+        # 创建 CSV writer，指定分隔符为逗号
+        csv_writer = csv.writer(csvfile, delimiter=',')
+        # 写入 CSV 文件的标题（假设为 "image_path" 和 "label"）
+        csv_writer.writerow(['image_name', 'target'])
+        # 遍历文件夹中的文件并将文件名（去掉后缀）和列表元素写入 CSV 文件
+        for file in files:
+            # 使用 os.path.splitext 获取文件名和扩展名
+            file_name, file_extension = os.path.splitext(file)
+            # 检查文件扩展名是否为 '.jpg'
+            if file_extension.lower() == '.jpg':
+                # 获取列表元素，如果列表为空或长度不够，则使用默认值 0
+                label = data_list.pop(0) if data_list else 0
+                # 将文件名（去掉后缀）和列表元素写入 CSV 文件的一行
+                csv_writer.writerow([file_name, label])
+
 
 # class 1=>584, class 0=>32542
 class MyNewDataset(Dataset):
@@ -31,56 +52,9 @@ class MyNewDataset(Dataset):
         img_path+='.jpg'
         imgs=read_image(img_path)
         label=self.img_labels.iloc[item,1]
-        
         if self.transforms:
             imgs=self.transforms(imgs)
-        
         return imgs,label
-
-
-
-class MyDataset(Dataset):
-    def __init__(self, x=None, y=None, filepath=None,transforms=None):
-        if filepath:
-            xy = np.loadtxt(filepath, delimiter=',', dtype=np.float32)
-            self.x = torch.from_numpy(xy[:, 1:])
-            self.y = torch.from_numpy(xy[:, [0]])
-            self.y = torch.flatten(self.y)
-        else:
-            self.x = x
-            self.y = y
-        self.len = self.x.shape[0] if self.x is not None else 0
-        # total length
-        print(f"length return= {self.len}")
-        # find the right label order
-        self.res = []
-        self.list=self.y.numpy()
-        self.list=self.list.tolist()
-        self.list = list(map(int, self.list[:]))
-        for j in self.list:
-            if j not in self.res:
-                self.res.append(j)
-        print(f"class = {self.res}")
-        print("Data prepared--------------------------------------------------------------------")
-
-        # Set the transform for data augmentation
-        self.transforms = transforms
-
-    #prepare index: dataset[index]
-    def __getitem__(self, index):
-        x,y= self.x[index],self.y[index]
-
-        # Reshape the flattened data to the original image shape
-        x = x.view(3, 74, 74)  # Assuming images are 3-channel and size 74x74, adjust accordingly
-        # Apply data augmentation only if the label is 1
-        if y == 1 and self.transforms is not None:
-            x = self.transforms(x)
-            
-
-        return x,y
-    #prepare length: len(dataset)
-    def __len__(self):
-        return self.len
 
 # Define data augmentation transform
 augmentation_transform = transforms.Compose([
@@ -91,29 +65,7 @@ augmentation_transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-
-# 创建一个函数来对少数类别进行过采样
-def oversample_data(dataset):
-    labels = dataset.label.numpy()
-
-    # 使用 RandomOverSampler 对少数类别进行过采样
-    oversampler = RandomOverSampler(sampling_strategy='minority', random_state=20)
-    x_resampled, y_resampled = oversampler.fit_resample(dataset.image.numpy(), labels)
-
-    # 转换为 PyTorch 张量
-    x_resampled = torch.from_numpy(x_resampled)
-    y_resampled = torch.from_numpy(y_resampled)
-    print(f"x_reshaped={x_resampled.shape}")
-
-    # 使用过采样后的数据创建新的数据集
-    oversampled_dataset = MyNewDataset(image=x_resampled, label=y_resampled,filepath=None)
-    return oversampled_dataset
-    
-"""   
-labels_csv = 'testLabel.csv'
-trainFolderPath='train-resizedtest'
-trainImageData=process_images(trainFolderPath,labels_csv)
-"""
+# Define my dataset
 
 label_csv = 'modified.csv'
 trainFolder='real-resized'
@@ -122,40 +74,12 @@ myData = MyNewDataset(label_csv=label_csv,root_dir=trainFolder,transforms=augmen
 #train_data = oversample_data(train_data_origin)
 
 train_data,val_data=torch.utils.data.random_split(myData,[0.9,0.1])
-#print(train_data)
-#train_over_sampling_data=oversample_data(train_data)
-
-# class 1=>584, class 0=>32542
-
-#samples_weights = weights[train_targets]
-#class_weights = [1/584, 1/32542]  # replace with your actual class weights
-# Create sample weights from class weights
-
-
-class_weights = [1/584, 1/32542]
-
-# Get the labels for the training data
-#y_train_indices = train_data.indices
-#print(y_train_indices)
-#print(len(train_data))
-#print(train_data[1][])
-#y_train = [train_data[i][1] for i in y_train_indices]
-
-# Create sample weights from class weights
-#samples_weight = [class_weights[t] for t in y_train]
-#sampler_weights means the probability of each sample (for all 33126 samples)
-# Create the sampler
-#mySampler = torch.utils.data.WeightedRandomSampler(weights=samples_weight.type('torch.DoubleTensor'), num_samples=len(samples_weight), replacement=True)
 train_loader = DataLoader(dataset=train_data,
                           batch_size=25,
                           shuffle=True,
                           drop_last=True)
 
 
-"""
-test_file="test.csv"
-test_data=MyNewDataset(label_csv=label_csv,root_dir=trainFolder)
-"""
 test_lst=[0,1]
 val_loader=DataLoader(dataset=val_data,
                         batch_size=25,
@@ -168,7 +92,7 @@ model = models.resnet50(pretrained=False)
 print(f"Origine Model= {model}")
 
 for param in model.parameters():
-    param.requires_grad = True  # 原模型中的参数冻结，不进行梯度更新
+    param.requires_grad = True  
 
 model.fc=torch.nn.Linear(2048,2)
 print(f"Modified Model= {model}")
@@ -210,8 +134,6 @@ for i in range(epoch):
         imgs,targets=imgs.to(device),targets.to(device)
         # imgs=imgs.reshape(20,3,74,74)  #batch,channel,img.height, img.weight
         outputs=model(imgs)
-        # print(f"outputs: {outputs}")
-        # print(f"targets: {targets}")
 
         #Add class weight to loss function
         loss=loss_func(outputs,targets.long())
@@ -226,7 +148,7 @@ for i in range(epoch):
             print("Train step {}, Loss: {}".format(total_train_step,loss))
             # writer.add_scalar("train_loss",loss.item(),total_train_step)
 
-    #start testing
+    #start validating
     # Create model
     target_name=[str(i) for i in test_lst]
     # print(f"target_name= {target_name}")
@@ -234,7 +156,6 @@ for i in range(epoch):
         for l,data in enumerate(val_loader):
             imgs,targets=data
             imgs,targets=imgs.to(device),targets.to(device)
-
             outputs=model(imgs)
             # outputs = cifar_train(imgs)
 
@@ -265,5 +186,29 @@ for i in range(epoch):
     # save trained model
     # torch.save(cifar_train,"cifar_{}.pth".format(i))
     # print("Model saved...")
+# start testing
+print("---------------start testing-----------------")
+testFolder='test-resized'
+testLabel='output.csv'
+finalResult=[]
+myTestData = MyNewDataset(label_csv=testLabel,root_dir=testFolder)
+testDataloader=DataLoader(dataset=myTestData,
+                        batch_size=25,
+                        shuffle=False,
+                        drop_last=False)
+with torch.no_grad():    #ensure this data would not be optimized
+    for _,data in enumerate(testDataloader):
+        imgs,_=data
+        imgs=imgs.to(device)
+        #print(testDataloader)
+        outputs=model(imgs.float())
+        outputs_np=outputs.cpu().numpy()
+        #Decide the prediction of outputs --[predict probability of each class]
+        x_predict=np.argmax(outputs_np,axis=1)
+        x_predict=x_predict.tolist()
+        finalResult.extend(x_predict)
+resultCSV='result.csv'
+
+outputCSV(testFolder,resultCSV,finalResult)
 
 writer.close()
